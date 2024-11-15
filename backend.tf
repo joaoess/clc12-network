f408f0e0f256bc74d50dc616a26ce44bfdedcd7d
João Silva <joaoess@hotmail.com>
2024-11-15 19:50:24 -0300

    Change bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "clc12-network-edison-maciel2"
+    bucket = "clc12-network-joaoess"
     key    = "newtork/terraform.tfstate"
     region = "us-east-1"
   }
